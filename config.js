aaf87b129009b8674d203b43dacc2c93a494f297
dautruongybb <30793156+dautruongybb@users.noreply.github.com>
2022-10-22 16:37:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello Mẹ ShuShi iu dấu!",
+  introDesc: `Phụ nữ cũng là một nửa bầu trời.\n
+    Nhưng từ khi có em, anh có cả thế giới.\n
+    Em yêu, chúc em ngày 20/10 luôn hạnh phúc,\n
+    vui vẻ và xinh đẹp nhé!`,
+  btnIntro: "hihi",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Vẫn cứ là iu anh <33",
+  btnNo: "Không, anh bị đin à >.<",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Đi cùng anh đến tận cùng thời gian nhé babiii :3",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://m.me/trungkien.love", //link mess của các bạn. VD: https://m.me/trungkien.love
+};
